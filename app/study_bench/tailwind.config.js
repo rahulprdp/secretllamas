7f885c1205d4a52e988f805c058d054ef24bbe6e
Rahul Pradeep <realrahul02@gmail.com>
2024-04-09 16:35:06 +0530

    feat: Front end setup wth angular and tailwind

---
+++ app/study_bench/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        primary : "Space Mono, monospace",
+        secondary :  "Pixelify Sans"
+      }
+    },
+  },
+  plugins: [],
+}
